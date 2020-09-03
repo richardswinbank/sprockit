5c82df2e3038564ea2fb74681896a615f35810e5
Richard Swinbank <richard@richardswinbank.net>
2020-09-03 16:36:08 +0100

    Update PrepareNewBatch.sql

--- vs/Sprockit/sprockit/Stored Procedures/PrepareNewBatch.sql
+++ vs/Sprockit/sprockit/Stored Procedures/PrepareNewBatch.sql
@@ -18,6 +18,7 @@ EXEC sprockit.UpdateMetrics @processGroup
 UPDATE p
 SET [Status] = 'Not ready'
   , LastStatusUpdate = GETUTCDATE()
+  , ErrorCount = 0
 FROM sprockit.Process p
 WHERE p.ProcessGroup = @processGroup
 
@@ -25,6 +26,7 @@ WHERE p.ProcessGroup = @processGroup
 UPDATE p
 SET [Status] = 'Ready'
   , LastStatusUpdate = GETUTCDATE()
+  , ErrorCount = 0
 FROM sprockit.Process p
   LEFT JOIN sprockit.ProcessDependency pd ON pd.ProcessId = p.ProcessId
 WHERE p.ProcessGroup = @processGroup
