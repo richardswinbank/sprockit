45681b86b1a09206e4a43ad3ddb65d9cb39b1e61
Richard Swinbank <richard@firefive.io>
2020-10-02 15:57:08 +0100

    Added test for defaultwatermark

---
+++ vs/Sprockit.Tests/sprockit_Process/test ASSERT DefaultWatermark is nullable.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE sprockit_Process.[test ASSERT DefaultWatermark is nullable]
+AS
+
+--ACT
+INSERT INTO [sprockit].[Process] (
+  [ProcessGroup]
+, [ProcessPath]
+, [ProcessType]
+, [IsValidator]
+, [Status]
+, [ErrorCount]
+, [LastStatusUpdate]
+, [LastExecutionId]
+, [DefaultWatermark]
+, [CurrentWatermark]
+, [AvgDuration]
+, [BranchWeight]
+, [IsEnabled]
+, [Priority]
+)
+SELECT 
+  [ProcessGroup]
+, '[My].[Process].[Path]'
+, [ProcessType]
+, 0
+, 'Done'
+, 0
+, GETUTCDATE()
+, 75
+, NULL
+, 'watermark'
+, 467
+, 794
+, 1
+, 90
+FROM sprockit_Process.TestParams
+
+-- ASSERT
+-- (no error)
+
