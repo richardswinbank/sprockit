af7ea2d0918606a5523a929d70a79d8279556c3a
Richard Swinbank <richard@firefive.io>
2021-07-04 14:11:26 +0100

    Added table structure tests

--- SprockitDb/Sprockit/sprockit/Tables/ProcessDependency.sql
+++ SprockitDb/Sprockit/sprockit/Tables/ProcessDependency.sql
@@ -2,7 +2,7 @@ CREATE TABLE [sprockit].[ProcessDependency] (
     [DependencyId] INT IDENTITY (1, 1) NOT NULL,
     [ProcessId]    INT NOT NULL,
     [DependsOn]    INT NOT NULL,
-    CONSTRAINT [PK__sprockit_Dependency] PRIMARY KEY NONCLUSTERED ([DependencyId] ASC),
+    CONSTRAINT [PK__sprockit_ProcessDependency] PRIMARY KEY NONCLUSTERED ([DependencyId] ASC),
     CONSTRAINT [CK__sprockit_ProcessDependency__NoSelfDependency] CHECK ([ProcessId]<>[DependsOn]),
     CONSTRAINT [FK__DependsOn] FOREIGN KEY ([DependsOn]) REFERENCES [sprockit].[Process] ([ProcessId]),
     CONSTRAINT [FK__ProcessId] FOREIGN KEY ([ProcessId]) REFERENCES [sprockit].[Process] ([ProcessId]),
