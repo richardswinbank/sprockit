a1e2d5ca2c040594bad6dce80c287e47770766e5
Richard Swinbank <richard@firefive.io>
2021-07-12 09:30:18 +0100

    Updated tests for new batchW

---
+++ SprockitDb/Sprockit.Tests/sprockit_PrepareNewBatch/test WHEN run THEN _internal_ManageLogs called once.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE sprockit_PrepareNewBatch.[test WHEN run THEN _internal_ManageLogs called once]
+AS
+
+-- ARRANGE
+DECLARE @processGroup INT = 1
+
+EXEC tSQLt.SpyProcedure 'sprockit._internal_ManageLogs'
+
+DECLARE @expectedCalls INT = 1;
+DECLARE @actualCalls INT = 0;
+
+-- ACT
+EXEC sprockit.PrepareNewBatch 
+  @processGroup = @processGroup
+
+-- ASSERT
+SELECT
+  @actualCalls = COUNT(*)
+FROM sprockit._internal_ManageLogs_SpyProcedureLog;
+
+EXEC tSQLt.AssertEquals
+  @Expected = @expectedCalls
+, @Actual = @actualCalls
---
+++ SprockitDb/Sprockit.Tests/sprockit_PrepareNewBatch/test WHEN run THEN _internal_ManageLogs called with parameter.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE sprockit_PrepareNewBatch.[test WHEN run THEN _internal_ManageLogs called with parameter]
+AS
+
+-- ARRANGE
+DECLARE @processGroup INT = 762353746;
+DECLARE @expected INT = @processGroup;
+
+EXEC tSQLt.SpyProcedure 'sprockit._internal_UpdateMetrics'
+
+DECLARE @actual INT = 0;
+
+-- ACT
+EXEC sprockit.PrepareNewBatch 
+  @processGroup = @processGroup;
+
+-- ASSERT
+SELECT
+  @actual = processGroup
+FROM sprockit._internal_UpdateMetrics_SpyProcedureLog;
+
+EXEC tSQLt.AssertEquals
+  @Expected = @expected
+, @Actual = @actual;
